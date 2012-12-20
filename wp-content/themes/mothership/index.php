6f39a11c16e4d8fca64c25b33d8a629368b7088c
gabeshaughnessy <gabeshaughnessy@gmail.com>
2012-12-20 15:54:51 -0800

    added title to page template

--- wp-content/themes/mothership/index.php
+++ wp-content/themes/mothership/index.php
@@ -1,2 +1,28 @@
+<?php
+/**
+ * The default page for our theme.
+ * 
+ *
+ * @package WordPress
+ * @subpackage mothership
+ * @since The Deconstruction Mothership 1.0
+ */
+?>
 <?php get_header(); ?>
+<div id="content" class="nine columns">
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<article>
+	<div class="post-title ">
+		<h2><?php the_title(); ?></h2>
+	</div>
+	<div class="post-content">
+		<?php the_content(); ?>
+	</div>
+</article>
+<?php endwhile; ?>
+<?php endif; ?>
+</div><!-- end of content -->
+	<?php get_template_part('sidebar'); ?>
+</div><!-- end of main content container -->
+
 <?php get_footer(); ?>
