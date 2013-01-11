3cf7b83358db8843460b8d82cdc9a33c8894d9ce
kgstew <kgstew@gmail.com>
2013-01-10 20:30:24 -0800

    added new header logo and changed size

--- wp-content/themes/mothership/header.php
+++ wp-content/themes/mothership/header.php
@@ -65,7 +65,7 @@ $theme_options = get_option('mothership_options');
 	</div>
 	
 	<!-- <div class="logo twelve columns last"><h1><?php bloginfo('name'); ?></h1></div> -->
-	<div class="logo seven columns centered"><h1><img src="wp-content/themes/mothership/images/deconlogo528x101.jpg"/></h1></div>
+	<div class="logo seven columns centered"><h1><img src="wp-content/themes/mothership/images/deconstruction_logo_1200x196.jpg"/></h1></div>
 	<div class="description nine columns centered"><p><?php bloginfo('description'); ?></p></div>
 </header>
 <div id="main-content-area" class="row">
