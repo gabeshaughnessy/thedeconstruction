7702bbce860b552e77de6a84b9dafa22c6553dfc
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-02-20 00:07:35 -0800

    added 404 content

--- wp-content/themes/mothership/404.php
+++ wp-content/themes/mothership/404.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * TEMPLATE NAME: 404 template
- *
+  *
  *
  * @package WordPress
  * @subpackage 1H1H
@@ -11,13 +11,15 @@ get_header(); ?>
 <div id="content" class="twelve columns">
 
 <hr>
-<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 <article>
-<h3><?php the_title(); ?></h3>
+<h3>404 Error! Page Not Found</h3>
 	<div class="post-content">
-		<?php the_content(); ?>
+		The page you are looking for does not exist.
+		Maybe you'd like to <a title="Register a Team" href="/register" class="has-tip">build it?</a>
 	</div>
 </article>
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
 <?php endwhile; ?>
 <?php endif; ?>
 </div><!-- end of content -->	
