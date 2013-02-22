251adec07ca8d97aee6e22836263e07b30ac5ad8
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-02-22 12:23:35 -0800

    removed social widget from the news feed

--- wp-content/themes/mothership/home.php
+++ wp-content/themes/mothership/home.php
@@ -26,7 +26,7 @@
 	 <div class="one columns offset-by-eleven">
 	 <?php 
 	 //get_template_part('sidebar'); 
-	 get_template_part('accordion', 'social');
+	 //get_template_part('accordion', 'social');
 	 ?>
 	 </div>
 </div><!-- end of main content container -->
