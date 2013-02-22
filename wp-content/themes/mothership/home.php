1795f4e9950b32c49b28701a5f131d2f58db9e45
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-02-22 12:36:47 -0800

    added author and profile thumb to news feed

--- wp-content/themes/mothership/home.php
+++ wp-content/themes/mothership/home.php
@@ -14,7 +14,7 @@
 <article>
 	<div class="post-title">
 		<h2><a href="<?php the_permalink(); ?>" title="View Post"><?php the_title(); ?></a></h2>
-		<p class="author"><em>posted by: </em><?php the_author_posts_link(); ?> on <?php echo the_date('F j, Y', '<em>','</em>', true); ?></p>
+			<div class="row"><div class="team-image one columns"><?php echo get_wp_user_avatar(get_the_author_meta('ID'), 'thumbnail');  ?></div><p class="author eleven columns end"><em>posted by: </em><?php the_author_posts_link(); ?> on <?php echo the_date('F j, Y', '<em>','</em>', true); ?></p></div>
 	</div>
 	<div class="post-content">
 		<?php the_content(); ?>
