378a30d12371010d242f53a4dfd05d02b8d2819f
gabeshaughnessy <gabeshaughnessy@gmail.com>
2016-09-16 12:34:14 -0700

    no sidebar on homepage

--- wp-content/themes/mothership/front-page.php
+++ wp-content/themes/mothership/front-page.php
@@ -12,7 +12,7 @@ global $show_menu;
 $show_menu = true;
 ?>
 <?php get_header(); ?>
-<div id="content" class="large-11 columns">
+<div id="content" class="large-12 columns">
 <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 <article>
 	<div class="post-content">
@@ -22,13 +22,6 @@ $show_menu = true;
 <?php endwhile; ?>
 <?php endif; ?>
 </div><!-- end of content -->
-
-	 <div class="large-1 columns offset-large-11">
-	 <?php 
-	 //get_template_part('sidebar'); 
-	 get_template_part('accordion', 'social');
-	 ?>
-	 </div>
 </div><!-- end of main content container -->
 
 <?php get_footer(); ?>
